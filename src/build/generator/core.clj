e067e12f3f547deb61166ab0b389b86d7482ddf5
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-09-16 18:44:19 +0300

    move generators to postinstall to enhance build performance

--- src/build/generator/core.clj
+++ src/build/generator/core.clj
@@ -2,9 +2,11 @@
   (:require
    [build.generator.bcd :as generator.bcd]
    [build.generator.icon :as generator.icon]
+   [build.generator.schema-graph :as generator.schema-graph]
    [build.generator.shell-dsl :as generator.shell-dsl]))
 
 (defn -main []
   (generator.icon/generate!)
   (generator.bcd/generate!)
-  (generator.shell-dsl/generate!))
+  (generator.shell-dsl/generate!)
+  (generator.schema-graph/generate!))
